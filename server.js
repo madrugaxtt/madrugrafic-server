ec9a0db600ae7003029ec534ec7feb5f563e4157
madruga <madrugaxtt@gmail.com>
2026-05-09 17:40:25 -0300

    rota inicial

--- server.js
+++ server.js
@@ -63,3 +63,6 @@ app.get('/block/:key', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
